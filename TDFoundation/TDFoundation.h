1d7d5d5a965af63073e4124b1af31e36aa06016f
Robin Hsu <robinhsu599+dev@gmail.com>
2015-03-16 22:45:28 +0800

    Initial Commit

---
+++ TDFoundation/TDFoundation.h
@@ -0,0 +1,13 @@
+//
+//  TDFoundation.h
+//  TDFoundation
+//
+//  Created by Robin Hsu on 2015/3/16.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TDFoundation : NSObject
+
+@end
---
+++ TDFoundation/TDFoundation.m
@@ -0,0 +1,13 @@
+//
+//  TDFoundation.m
+//  TDFoundation
+//
+//  Created by Robin Hsu on 2015/3/16.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import "TDFoundation.h"
+
+@implementation TDFoundation
+
+@end
